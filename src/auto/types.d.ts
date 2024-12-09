6ff9fdfd4117bf832bccd59fc2c8107b9fa85c79
eXory2024 <exory@anio.software>
2024-12-09 14:34:13 +0100

    Remove obsolete files.

--- src/auto/types.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "@anio-fs/int--hash-file"
-export * from "@anio-fs/int--read-file-in-chunks"
-export * from "@anio-fs/int--write-atomic-file"
-export * from "@anio-fs/int--find-nearest-file"
-export * from "@anio-fs/int--write-atomic-file-json"
