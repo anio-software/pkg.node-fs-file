887bda7eca41682a079ea5db51a46c37888e068a
eXory2024 <exory@anio.software>
2025-06-26 19:15:20 +0200

    No longer needed to rename imported dependency - enkore does that for us now! :)

--- project/src/__openFileXXX.as.ts
+++ project/src/__openFileXXX.as.ts
@@ -2,8 +2,8 @@ import {
 	type EnkoreJSRuntimeContextOptions
 } from "@anio-software/enkore.js-runtime"
 
-import {openFile as x} from "@anio-software/pkg-private.node-consistent-fs/async"
-//>import {openFile as x} from "@anio-software/pkg-private.node-consistent-fs/sync"
+import {openFile} from "@anio-software/pkg-private.node-consistent-fs/async"
+//>import {openFile} from "@anio-software/pkg-private.node-consistent-fs/sync"
 
 export async function __implementation(
 //>export function __implementationSync(
@@ -11,8 +11,8 @@ export async function __implementation(
 	path: string,
 	flags?: string,
 	mode?: number
-) : Promise<ReturnType<typeof x>> {
-//>) : ReturnType<typeof x> {
-	return await x(path, flags, mode)
-//>	return x(path, flags, mode)
+) : Promise<ReturnType<typeof openFile>> {
+//>) : ReturnType<typeof openFile> {
+	return await openFile(path, flags, mode)
+//>	return openFile(path, flags, mode)
 }
