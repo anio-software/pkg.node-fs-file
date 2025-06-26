c29e130fc46f95970ff57cd5663c6d28214f693b
eXory2024 <exory@anio.software>
2025-06-26 18:53:32 +0200

    Migrate from camel_case to camelCase.

--- project/src/__findNearestFileXXX.as.ts
+++ project/src/__findNearestFileXXX.as.ts
@@ -8,12 +8,12 @@ import {nodeFsFindNearestFile} from "@anio-software/pkg-private.node-fs-find-nea
 export async function __implementation(
 //>export function __implementationSync(
 	contextOptions: EnkoreJSRuntimeContextOptions,
-	config_file_name: string,
-	start_dir: string
+	configFileName: string,
+	startDir: string
 ) : Promise<string|false> {
 //>) : string|false {
 	return await nodeFsFindNearestFile(
 //>	return nodeFsFindNearestFile(
-		config_file_name, start_dir
+		configFileName, startDir
 	)
 }
