94094340426eaeace357d31904ed94240a429d19
eXory2024 <exory@anio.software>
2025-06-26 18:53:11 +0200

    Context isn't used here.

--- project/src/__findNearestFileXXX.as.ts
+++ project/src/__findNearestFileXXX.as.ts
@@ -1,6 +1,5 @@
 import {
-	type EnkoreJSRuntimeContextOptions,
-	createContext
+	type EnkoreJSRuntimeContextOptions
 } from "@anio-software/enkore.js-runtime"
 
 import {nodeFsFindNearestFile} from "@anio-software/pkg-private.node-fs-find-nearest-file"
@@ -13,8 +12,6 @@ export async function __implementation(
 	start_dir: string
 ) : Promise<string|false> {
 //>) : string|false {
-	const context = createContext(contextOptions, 0)
-
 	return await nodeFsFindNearestFile(
 //>	return nodeFsFindNearestFile(
 		config_file_name, start_dir
