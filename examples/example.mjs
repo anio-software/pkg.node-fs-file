01a9e9b4cc00633993a0500fd5599ca88c765eb2
eXory2024 <exory@anio.software>
2025-06-26 19:07:31 +0200

    Update example.

--- examples/example.mjs
+++ examples/example.mjs
@@ -1,4 +1,4 @@
-import {hashFileSync, readFileInChunksSync} from "../dist/default/index.min.mjs"
+import {hashFileSync, readFileInChunksSync} from "../products/project/dist/default/index.min.mjs"
 
 console.log(
 	hashFileSync("./package.json", "sha1"),
